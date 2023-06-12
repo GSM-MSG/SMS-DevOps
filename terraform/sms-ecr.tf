80561fb5b5d9471f43ec462a96a35d6038aa14fe
yohan <noyohanx@naver.com>
2023-06-12 17:44:39 +0900

    :poop: :: CI 테스트

--- terraform/sms-ecr.tf
+++ terraform/sms-ecr.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "sms-ecr" {
-    name = "sms-ecr"
+resource "aws_ecr_repository" "sms-ecr-repo" {
+    name = "sms-ecr-repo"
     image_tag_mutability = "MUTABLE"
     image_scanning_configuration {
         scan_on_push = true
