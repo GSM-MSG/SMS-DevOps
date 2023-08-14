913319422d648c7c691b825262d57b4cece226f2
yohan <noyohanx@naver.com>
2023-08-14 13:09:41 +0900

    :bulb: :: 전체 주석처리

--- terraform/sms-ec2.tf
+++ terraform/sms-ec2.tf
@@ -38,45 +38,45 @@
 # }
 
 ## Create Main Server SG
-resource "aws_security_group" "sms-main-server-sg" {
-    vpc_id = "${aws_vpc.sms-vpc.id}"
+# resource "aws_security_group" "sms-main-server-sg" {
+#     vpc_id = "${aws_vpc.sms-vpc.id}"
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        security_groups = [aws_security_group.sms-bastion-sg.id]
-    }
+#     ingress {
+#         from_port = 22
+#         to_port = 22
+#         protocol = "tcp"
+#         security_groups = [aws_security_group.sms-bastion-sg.id]
+#     }
 
-    ingress{
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+#     ingress{
+#         from_port = 8080
+#         to_port = 8080
+#         protocol = "tcp"
+#         cidr_blocks     = ["0.0.0.0/0"]
+#     }
 
-    ingress{
-        from_port = 6379
-        to_port = 6379
-        protocol = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+#     ingress{
+#         from_port = 6379
+#         to_port = 6379
+#         protocol = "tcp"
+#         cidr_blocks     = ["0.0.0.0/0"]
+#     }
 
-    ingress{
-        from_port = 587
-        to_port = 587
-        protocol = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+#     ingress{
+#         from_port = 587
+#         to_port = 587
+#         protocol = "tcp"
+#         cidr_blocks     = ["0.0.0.0/0"]
+#     }
 
-    egress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+#     egress {
+#         from_port       = 0
+#         to_port         = 0
+#         protocol        = "-1"
+#         cidr_blocks     = ["0.0.0.0/0"]
+#     }
 
-    tags = {
-        Name = "sms-main-server-sg"
-    }
-}
+#     tags = {
+#         Name = "sms-main-server-sg"
+#     }
+# }
