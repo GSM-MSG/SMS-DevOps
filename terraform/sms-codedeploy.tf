83d54c64758f3599a3a40064378f57ba8b7c9b4a
yohan <noyohanx@naver.com>
2023-08-14 13:08:00 +0900

    :sparkles: :: codedeploy 관련 iam 생성

--- terraform/sms-codedeploy.tf
+++ terraform/sms-codedeploy.tf
@@ -1,3 +1,22 @@
-resource "aws_codedeploy_app" "codedeploy-application" {
-    # (resource arguments)
+data "aws_iam_policy_document" "assume_role" {
+    statement {
+        effect = "Allow"
+
+    principals {
+        type        = "Service"
+        identifiers = ["codedeploy.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+    }
+}
+
+resource "aws_iam_role" "codedeploy-role" {
+    name               = "codedeploy-role"
+    assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+    role       = aws_iam_role.codedeploy-role.name
 }
