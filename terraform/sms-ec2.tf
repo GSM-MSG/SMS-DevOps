b3598639c3dfffd913cb859add65d6559420e57d
yohan <noyohanx@naver.com>
2023-08-14 11:31:24 +0900

    :bulb: :: 현재 ec2가 없는 관계로 주석처리

--- terraform/sms-ec2.tf
+++ terraform/sms-ec2.tf
@@ -1,53 +1,41 @@
 # Create EC2
 ## Create Bastion + Nat Instance
-resource "aws_instance" "sms-bastion" {
-    ami = "ami-04cebc8d6c4f297a3"
-    instance_type =  "t2.nano"
-    subnet_id = "${aws_subnet.sms-public-subnet-2a.id}"
-    vpc_security_group_ids = [aws_security_group.sms-bastion-sg.id]
-    key_name = "sms-key"
+# resource "aws_instance" "sms-main-server" {
+#     ami = "ami-04cebc8d6c4f297a3"
+#     instance_type = "t3.medium"
+#     subnet_id = "${aws_subnet.sms-private-subnet-2a.id}"
+#     vpc_security_group_ids = [aws_security_group.sms-main-server-sg.id]
+#     key_name = "sms-key"
+#     associate_public_ip_address = false
+#     source_dest_check = false
 
-    tags = {
-        Name = "sms-bastion"
-    }
-}
-
-resource "aws_instance" "sms-main-server" {
-    ami = "ami-04cebc8d6c4f297a3"
-    instance_type = "t3.medium"
-    subnet_id = "${aws_subnet.sms-private-subnet-2a.id}"
-    vpc_security_group_ids = [aws_security_group.sms-main-server-sg.id]
-    key_name = "sms-key"
-    associate_public_ip_address = false
-    source_dest_check = false
-
-    tags = {
-        Name = "sms-main-server"
-    }
-}   
+#     tags = {
+#         Name = "sms-main-server"
+#     }
+# }   
 
 # Create SG
 ## Create Bastion SG
-resource "aws_security_group" "sms-bastion-sg" {
-    vpc_id = "${aws_vpc.sms-vpc.id}"
+# resource "aws_security_group" "sms-bastion-sg" {
+#     vpc_id = "${aws_vpc.sms-vpc.id}"
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    }
+#     ingress {
+#         from_port = 22
+#         to_port = 22
+#         protocol = "tcp"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
+#     egress {
+#     from_port       = 0
+#     to_port         = 0
+#     protocol        = "-1"
+#     cidr_blocks     = ["0.0.0.0/0"]
+#     }
 
-    tags = {
-        Name = "sms-sg"
-    }
-}
+#     tags = {
+#         Name = "sms-sg"
+#     }
+# }
 
 ## Create Main Server SG
 resource "aws_security_group" "sms-main-server-sg" {
