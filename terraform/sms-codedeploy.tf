df13c7f8708111b3c59f1227e6adb08606ceb64f
yohan <noyohanx@naver.com>
2023-08-14 13:10:48 +0900

    :recycle: :: 테라폼 컴퓨팅 관련 리소스 리펙토링

--- terraform/sms-codedeploy.tf
+++ terraform/sms-codedeploy.tf
@@ -20,3 +20,14 @@ resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
     role       = aws_iam_role.codedeploy-role.name
 }
+
+resource "aws_codedeploy_app" "codedeploy-application" {
+    compute_platform = "Server"
+    name             = "codedeploy-application"
+}
+
+resource "aws_codedeploy_deployment_group" "codedeploy-group" {
+    app_name = aws_codedeploy_app.codedeploy-application.name
+    deployment_group_name = "codedeploy-group"
+    service_role_arn = aws_iam_role.codedeploy-role.arn
+}
