0c754293c4617286f8f423c021ea94c1f6b03843
HumanIearning <kimdh3896@naver.com>
2023-09-04 20:16:07 +0900

    :sparkles: :: 함수형에서 클래스형으로

--- automation/discord-bot/log/sunghun_bot.py
+++ automation/discord-bot/log/sunghun_bot.py
@@ -5,8 +5,7 @@ import dotenv
 import discord
 from discord.ext import commands
 
-from load_cloudwatch_log import load_log
-
+from log_pagination_view import LogPaginationView
 
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
@@ -17,14 +16,15 @@ bot = commands.Bot(command_prefix=("ㅅ ", "성훈아 "), intents=discord.Intent
 async def on_ready():
     print("이번 인수합병은 만족스러웠습니다")
 
-@bot.command(aliases=['ㄹ'])
-async def 로그보여줘(ctx, log_type, log_amount):
-    log = load_log(log_type, log_amount)
-    await ctx.send('\n'.join(log[0]))
-
 @bot.command()
 async def 안녕(ctx):
     await ctx.send('난 부자다 으하하하')
 
+
+@bot.command(aliases=['ㄹ'])
+async def 로그보여줘(ctx, log_type, log_amount):
+    view = LogPaginationView()
+    # log = load_log(log_type, log_amount)
+    await ctx.reply("아~ 귀찮게 또 뭘 시키는거야", view)
 
 bot.run(os.environ["TOKEN"])
