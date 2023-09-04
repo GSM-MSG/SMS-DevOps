997de59afa45051a5a4c641b19337aee252bbf8f
HumanIearning <kimdh3896@naver.com>
2023-09-04 20:16:26 +0900

    :sparkles: :: 이것이 모듈화?

---
+++ automation/discord-bot/log/log_pagination_view.py
@@ -0,0 +1,6 @@
+import discord
+
+class LogPaginationView(discord.ui.View):
+    @discord.ui.button(label="백엔드 로그 보여주라..", style=discord.ButtonStyle.grey)
+    async def fetch_backend_log(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(content="아 뭔 로그 볼껀데;;")
