199ab1079463b61134f424fabf1ddf918c28b5cf
HumanIearning <kimdh3896@naver.com>
2023-09-07 21:02:09 +0900

    :recycle: :: 뭐 수정했는지 까먹음..

--- automation/discord-bot/log/log_view.py
+++ automation/discord-bot/log/log_view.py
@@ -10,50 +10,47 @@ class LogLevelSelectionView(discord.ui.View):
         super().__init__(timeout=20)
         self.ctx = ctx
 
-    async def on_timeout(self) -> None:
-        await self.ctx.send("늦었어. 난 충분히 기다렸다.")
-
     @discord.ui.button(label="INFO", style=discord.ButtonStyle.grey, emoji="✅")
     async def show_info_log(self, interaction: discord.Interaction, button: discord.ui.Button):
-        log_data = load_log('INFO', 1000)
+        log_data = load_log('INFO', 100000)
         log_page = LogPaginationView(self.ctx, log_data)
         await log_page.send()
 
     @discord.ui.button(label="WARN", style=discord.ButtonStyle.grey, emoji="⚠️")
     async def show_warn_log(self, interaction: discord.Interaction, button: discord.ui.Button):
-        log_data = load_log('WARN', 1000)
+        log_data = load_log('WARN', 100000)
         log_page = LogPaginationView(self.ctx, log_data)
         await log_page.send()
 
     @discord.ui.button(label="ERROR", style=discord.ButtonStyle.grey, emoji="‼️")
     async def show_error_log(self, interaction: discord.Interaction, button: discord.ui.Button):
-        log_data = load_log('ERROR', 1000)
+        log_data = load_log('ERROR', 100000)
         log_page = LogPaginationView(self.ctx, log_data)
         await log_page.send()
 
     @discord.ui.button(label="다 보여줘!", style=discord.ButtonStyle.grey)
     async def show_all_log(self, interaction: discord.Interaction, button: discord.ui.Button):
-        log_data = load_log('ALL', 1000)
+        log_data = load_log('ALL', 100000)
         log_page = LogPaginationView(self.ctx, log_data)
         await log_page.send()
 
 
 class LogPaginationView(discord.ui.View):
     def __init__(self, ctx, data):
-        super().__init__(timeout=10)
-        self.ctx= ctx
+        super().__init__(timeout=25)
+        self.ctx = ctx
         self.data = data[0]
         self.data.reverse()
         self.metadata = data[1]
 
-        self.current_page : int = 1
-        self.sep : int = 10
+        self.current_page: int = 1
+        self.sep: int = 10
 
     async def on_timeout(self) -> None:
-        await self.ctx.send("갔냐?")
+        await self.ctx.send("갔냐? 나도 간다 ㅂㅂ")
 
     async def send(self):
-        self.message = await self.ctx.send(view=self)
+        self.message = await self.ctx.send(view=self, embed=self.create_embed(self.data[:self.sep]))
 
     def create_embed(self, data):
         embed = discord.Embed(title="Backend Log")
